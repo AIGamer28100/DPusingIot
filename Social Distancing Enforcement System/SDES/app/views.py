18f355a095f55f9c7206ab608e89baef249353a4
AIGAMER <hariharankvasn@gmail.com>
2021-07-30 00:23:27 +0530

    updates

--- Social Distancing Enforcement System/SDES/app/views.py
+++ Social Distancing Enforcement System/SDES/app/views.py
@@ -10,8 +10,8 @@ import cv2, base64
 
 class gen():
     def __init__(self, request):
-        self.session = request.session
-        self.camera = FaceDetect(self.session)
+        self.session = request
+        self.camera = FaceDetect(self.session.session)
 
     def frame(self):
         while True:
@@ -21,7 +21,7 @@ class gen():
                 yield(b'--frame\r\n'
                     b'Content_type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
             except:
-                continue
+                self.__init__(self.session)
 
 data = ""
 def read(request):
@@ -35,7 +35,7 @@ def Unread(request):
         try:
             data = request.POST.get("content").split(",")[1]
             with open(f"app/data/{request.session.session_key}-clientImage.png", 'wb') as f:
-                f.write(base64.decodebytes(data))
+                f.write(base64.b64decode(data))
         except:
             return HttpResponse(f"{data}")
     return HttpResponse(f"{data}")
